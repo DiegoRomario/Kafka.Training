bd5d129c5e0c784c604ebda4fcb2da2efaf89644
Diego Romario <diego.romario@outlook.com>
2020-05-09 07:50:00 -0300

    adding 3 partitions, 2 consumers and knowing ClientId

---
+++ src/1-IntroductionToStreamsInMicroservices/Consumer_two/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Consumer_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            var group = typeof(Program).Assembly.GetName().Name;
+            var config = new ConsumerConfig()
+            {
+                GroupId = group,
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                ClientId = "I'M CONSUMER TWO"
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+
+                consumer.Subscribe("DEMO_ORDER");
+
+                var token = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        ConsumeResult<string, string> message = consumer.Consume(token.Token);
+                        Console.WriteLine($"Received [{message.Message.Value}] to topic: {message.Topic} / partition: {message.Partition} / offset: {message.Offset} /");
+
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ src/1-IntroductionToStreamsInMicroservices/Producer_one/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer_one
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+            };
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Thread.Sleep(2500);
+                        var message = new Message<string, string>() { Value = $"Message Id: {Guid.NewGuid().ToString() }", Key = Guid.NewGuid().ToString() };
+                        DeliveryResult<string, string> demo = await producer.ProduceAsync("DEMO_ORDER", message);
+                        Console.WriteLine($"Sent to topic: {demo.Topic} / partition: {demo.Partition} / offset: {demo.Offset} ");
+
+
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"The message failed: {e.Error.Reason}");
+                }
+                Console.ReadKey();
+
+            }
+
+
+        }
+    }
+}
